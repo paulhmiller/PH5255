f7c267962e9ccbb3145b77de972420ea93d5c1a0
paulhmiller <paulhmiller@gmail.com>
2016-07-15 18:07:55 -0700

    update at end of week 1

--- packages.R
+++ packages.R
@@ -8,8 +8,8 @@ biocLite([missing package name here, in quotes])
 source("http://www.bioconductor.org/biocLite.R")
 biocLite()
 library(devtools)
-install_github("genomicsclass/ph525x")
 
+install_github("genomicsclass/ph525x")
 biocLite("bioBase")
 #biocLite("GSE5859")
 devtools::install_github("genomicsclass/GSE5859")
@@ -17,22 +17,8 @@ biocLite("annotate")
 biocLite("BiocParallel")
 biocLite("VariantAnnotation")
 biocLite("BSgenome.Hsapiens.UCSC.hg19")
-biocLite("SNPlocs.Hsapiens.dbSNP.20120608")
+#biocLite("SNPlocs.Hsapiens.dbSNP.20120608")
+biocLite("SNPlocs.Hsapiens.dbSNP144.GRCh37")
 biocLite("Homo.sapiens")
-
-
-
-library(Biobase)
-library(GSE5859)
-library(annotate)
-library(BiocParallel)
-library(VariantAnnotation)
-library(BSgenome.Hsapiens.UCSC.hg19)
-library(Homo.sapiens)
-
-
-
-
-
-
-
+biocLite("gwascat")
+install_github("genomicsclass/tissuesGeneExpression")
--- week1.R
+++ week1.R
@@ -1,15 +1,19 @@
-# The 70-gene diagnostic signature for breast cancer progression risk
-# genefu package
+# PH5255.5x Introduction to Bioconductor
+# Week 1
 
+
+# A review of what we measure
+#============================
+
+# The 70-gene diagnostic signature for breast cancer progression risk using genefu package
 library(genefu)
 data(sig.gene70)
 dim(sig.gene70)
 head(sig.gene70)[,1:6]
-
 sum(is.na(sig.gene70$NCBI.gene.symbol))
 grep("kinase", sig.gene70$Description)
 
-# checking normality and transforming data
+# Checking for gaussian normality and transforming data
 library(COPDSexualDimorphism.data)
 data(lgrc.expr.meta)
 sum(expr.meta$GENDER == "2-Female")
@@ -26,7 +30,7 @@ boxcox(lm1)
 lambda <- 0.5
 boxplot(I(pyp1^lambda)~gender, data=expr.meta)
 
-# exploring the genome
+# Exploring the genome
 library(BSgenome.Hsapiens.UCSC.hg19)
 BSgenome.Hsapiens.UCSC.hg19
 chr11seq <- BSgenome.Hsapiens.UCSC.hg19[["chr11"]]
@@ -40,4 +44,44 @@ countPattern("TAG", chr11seq)
 chr7seq <- BSgenome.Hsapiens.UCSC.hg19[["chr7"]]
 alphabetFrequency(chr7seq)
 31671670 / sum(alphabetFrequency(chr7seq))
+
+# Locations of SNPs in humans 
 library(SNPlocs.Hsapiens.dbSNP144.GRCh37)
+snps144 <- SNPlocs.Hsapiens.dbSNP144.GRCh37
+s17 <- snplocs(snps144, "ch17")
+head(s17)
+#What is the location on chr17 of SNP rs73971683?
+s17[s17$RefSNP_id == "73971683", ]
+
+# GWAS: Linking SNP genotypes to disease risk 
+library(gwascat)
+data(ebicat37)
+ebicat37
+sort(table(ebicat37$CHR_ID),decreasing=TRUE)
+head(sort(table(ebicat37$DISEASE.TRAIT),decreasing=TRUE))
+
+# Gene expression with microarrays
+library(tissuesGeneExpression)
+data(tissuesGeneExpression)
+head(e[,1:5])
+table(tissue)
+boxplot(e["209169_at", ] ~ tissue, las=2)
+IDs = c("201884_at", "209169_at", "206269_at", "207437_at", "219832_s_at", "212827_at")
+par(mfrow=c(2,3))
+for (i in IDs){
+  boxplot(e[i, ] ~ tissue, las=2, main=i)
+}
+
+# Discovery of microarray annotation in Bioconductor, search for chip ID on bioconductor
+# Oligo sequences on affymetrix arrays 
+library(BiocInstaller)
+biocLite("hgu133aprobe")
+library(hgu133aprobe)
+head(hgu133aprobe)
+nrow(hgu133aprobe[hgu133aprobe$Probe.Set.Name=="206269_at",])
+
+
+# Microarray Technology
+#======================
+
+
